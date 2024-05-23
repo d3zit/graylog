ecac6fd6d4d86e90742effcb6aeb2943b62c12f1
D3z <pinzanoaltagl@gmail.com>
2024-05-23 13:04:41 +0200

    permessi cartella graylog

--- folder_permissions.sh
+++ folder_permissions.sh
@@ -27,6 +27,12 @@ sudo chmod -R 755 "$OPENSEARCH_DATA_DIR"
 sudo chmod -R 755 "$GRAYLOG_DATA_DIR"
 sudo chmod -R 755 "$GRAYLOG_CONFIG_DIR"
 
+# Aggiungi permessi di scrittura alle directory specifiche
+sudo chmod -R 775 "$GRAYLOG_DATA_DIR"
+
+# Cambia temporaneamente la proprietà dei volumi per Graylog
+docker run --rm -v "$GRAYLOG_DATA_DIR:/usr/share/graylog/data" alpine chown -R 1100:1100 /usr/share/graylog/data
+
 # Esegui Docker Compose per avviare i container
 docker-compose up -d
 

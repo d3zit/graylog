d742816a0ffc2b5e60cefc1213ed8344a53a6379
D3z <pinzanoaltagl@gmail.com>
2024-05-23 18:57:32 +0200

    altro tentativo

--- run.sh
+++ run.sh
@@ -8,11 +8,6 @@ MONGO_DATA_DIR="${PROJECT_DIR}/data/mongo"
 OPENSEARCH_DATA_DIR="${PROJECT_DIR}/data/log"
 GRAYLOG_DATA_DIR="${PROJECT_DIR}/data/graylog_data"
 
-# Crea le directory se non esistono
-#mkdir -p "$MONGO_DATA_DIR"
-#mkdir -p "$OPENSEARCH_DATA_DIR"
-#mkdir -p "$GRAYLOG_DATA_DIR"
-
 
 # Sposta il file graylog.conf se esiste nella posizione vecchia
 #if [ -f "${PROJECT_DIR}/config/graylog/graylog.conf" ]; then
@@ -22,10 +17,11 @@ GRAYLOG_DATA_DIR="${PROJECT_DIR}/data/graylog_data"
 # Cambia il proprietario delle directory a UID 1000 e GID 1000
 sudo chown -R 1000:1000 "$MONGO_DATA_DIR"
 sudo chown -R 1000:1000 "$OPENSEARCH_DATA_DIR"
+#sudo chown -R 1000:1000 "$GRAYLOG_DATA_DIR"
+
 
 # Cambia il proprietario della directory Graylog a UID 1100 e GID 1100
 sudo chown -R 1100:1100 "$GRAYLOG_DATA_DIR"
-
 
 # Imposta i permessi sulle directory
 sudo chmod -R 755 "$MONGO_DATA_DIR"
